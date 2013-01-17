c6635d19e8b39ebed324c8df231c4de8a9caa6bf
Joe Mifsud <jamifsud@umich.edu>
2013-01-17 12:45:50 -0500

    remote shutdown

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -337,9 +337,11 @@ module Resque
 
     # Schedule this worker for shutdown. Will finish processing the
     # current job.
-    def shutdown
+    # If passed true, mark the shutdown in Redis to signal a remote shutdown
+    def shutdown(remote = false)
       log 'Exiting...'
       @shutdown = true
+      redis.set("worker:#{self}:shutdown", true) if remote
     end
 
     # Kill the child and shutdown immediately.
@@ -354,7 +356,7 @@ module Resque
 
     # Should this worker shutdown as soon as current job is finished?
     def shutdown?
-      @shutdown
+      @shutdown || redis.get("worker:#{self}:shutdown")
     end
 
     # Kills the forked child immediately, without remorse. The job it
@@ -467,6 +469,7 @@ module Resque
       redis.srem(:workers, self)
       redis.del("worker:#{self}")
       redis.del("worker:#{self}:started")
+      redis.del("worker:#{self}:shutdown")
 
       Stat.clear("processed:#{self}")
       Stat.clear("failed:#{self}")
